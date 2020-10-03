0d8f35852ff4e851c5363fd219def4825b18ea23
Victor Chen <victorchen2022@yahoo.com>
2020-10-03 13:15:29 -0400

    clickable map implemented, but not pretty

---
+++ components/map.js
@@ -0,0 +1,54 @@
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '1280px',
+  height: '800px'
+};
+
+const center = {
+  lat: 0,
+  lng: 0
+}
+
+export default class MapView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.mapClicked = this.mapClicked.bind(this);
+  }
+
+  mapClicked = e => {
+    // console.log(e.latLng.lat() + ", " + e.latLng.lng());
+    // this.setState({
+    //   location: {
+    //     lat: e.latLng.lat(),
+    //     lng: e.latLng.lng()
+    //   }
+    // });
+    // console.log(this.state.location);
+
+    this.props.setLocation(e.latLng.lat(), e.latLng.lng());
+  }
+
+  render() {
+    return (
+      <LoadScript
+      googleMapsApiKey="AIzaSyBbo43u_w3Ap59Rj4V66j8J2AQDuQicjM8"
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={1}
+          onClick={this.mapClicked}
+        >
+          <Marker
+          position={{
+            lat: this.props.location.lat,
+            lng: this.props.location.lng
+          }}
+          />
+        </GoogleMap>
+      </LoadScript>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import MapView from '../components/map.js'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    this.state = {
+      location: {
+        lat: null,
+        lng: null
+      }
+    }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    this.setLocation = this.setLocation.bind(this);
+  }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+  setLocation(lat, lng) {
+    this.setState({
+      location: {
+        lat: lat,
+        lng: lng
+      }
+    })
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    console.log(this.state.location);
+  }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Create Next App</title>
+        </Head>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <main>
+          <MapView location={this.state.location} setLocation={this.setLocation}/>
+          <div>lat: {this.state.location.lat}</div>
+          <div>lng: {this.state.location.lng}</div>
+        </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        <footer>
+        </footer>
+      </div>
+    )
+  }
 }
