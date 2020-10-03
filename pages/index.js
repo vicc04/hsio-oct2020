fd00f4aab9ba4ffb9395bba163b90d9dd090804b
Victor Chen <victorchen2022@yahoo.com>
2020-10-03 14:19:53 -0400

    added reverse geocoding to output zip code

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import MapView from '../components/map.js'
+import { Client as GoogleMapsClient } from "@googlemaps/google-maps-services-js";
+
+const mapsClient = new GoogleMapsClient({});
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -8,7 +11,8 @@ export default class Home extends React.Component {
     this.state = {
       location: {
         lat: null,
-        lng: null
+        lng: null,
+        zip: null
       }
     }
 
@@ -16,14 +20,25 @@ export default class Home extends React.Component {
   }
 
   setLocation(lat, lng) {
-    this.setState({
-      location: {
-        lat: lat,
-        lng: lng
-      }
-    })
-
-    console.log(this.state.location);
+    mapsClient.reverseGeocode({
+      params: {
+        key: "AIzaSyBbo43u_w3Ap59Rj4V66j8J2AQDuQicjM8",
+        latlng: { lat: lat, lng: lng },
+        result_type: 'postal_code'
+      },
+      headers: null
+    }).then(r => {
+      this.setState({
+        location: {
+          lat: lat,
+          lng: lng,
+          zip: r.data.results[0].address_components[0].long_name
+        }
+      });
+      console.log(this.state.location);
+    }).catch(e => {
+      console.log(e);
+    });
   }
 
   render() {
@@ -34,9 +49,10 @@ export default class Home extends React.Component {
         </Head>
 
         <main>
-          <MapView location={this.state.location} setLocation={this.setLocation}/>
+          <MapView location={this.state.location} setLocation={this.setLocation} />
           <div>lat: {this.state.location.lat}</div>
           <div>lng: {this.state.location.lng}</div>
+          <div>zip: {this.state.location.zip}</div>
         </main>
 
         <footer>
